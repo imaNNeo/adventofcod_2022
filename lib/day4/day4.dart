07106e36c05146fda6cc1f6b528b497a8908d2a5
imaNNeoFighT <iman.neofight@gmail.com>
2022-12-04 08:56:32 +0330

    Solve day4 problem

---
+++ lib/day4/day4.dart
@@ -0,0 +1,25 @@
+import 'dart:io';
+
+Future<String> solveDay4() async {
+  final inputFile = File('lib/day4/input.txt');
+  final lines = await inputFile.readAsLines();
+
+  int counter = 0;
+  for (final line in lines) {
+    final firstFrom = int.parse(line.split(',')[0].split('-')[0]);
+    final firstTo = int.parse(line.split(',')[0].split('-')[1]);
+
+    final secondFrom = int.parse(line.split(',')[1].split('-')[0]);
+    final secondTo = int.parse(line.split(',')[1].split('-')[1]);
+
+    if ((firstFrom >= secondFrom && firstFrom <= secondTo) ||
+        (firstTo >= secondFrom && firstTo <= secondTo)) {
+      counter += 1;
+    } else if ((secondFrom >= firstFrom && secondFrom <= firstTo) ||
+        (secondTo >= firstFrom && secondTo <= firstTo)) {
+      counter += 1;
+    }
+  }
+
+  return '$counter';
+}
