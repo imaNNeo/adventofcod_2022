3183b9c89931e7e896b6bd4e55b800f144d85279
imaNNeoFighT <iman.neofight@gmail.com>
2022-12-06 08:46:47 +0330

    Solve day6 problem

--- bin/adventofcode.dart
+++ bin/adventofcode.dart
@@ -3,6 +3,7 @@ import 'package:adventofcode/day2/day2.dart';
 import 'package:adventofcode/day3/day2.dart';
 import 'package:adventofcode/day4/day4.dart';
 import 'package:adventofcode/day5/day5.dart';
+import 'package:adventofcode/day6/day6.dart';
 
 void main(List<String> arguments) async {
   print('Day1 answer is: ${await solveDay1()}');
@@ -10,4 +11,5 @@ void main(List<String> arguments) async {
   print('Day3 answer is: ${await solveDay3()}');
   print('Day4 answer is: ${await solveDay4()}');
   print('Day5 answer is: ${await solveDay5()}');
+  print('Day6 answer is: ${await solveDay6()}');
 }
