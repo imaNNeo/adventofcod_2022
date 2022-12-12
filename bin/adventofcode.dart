4a6be2a985de602b46581736702b8893059e0ce1
imaNNeoFighT <iman.neofight@gmail.com>
2022-12-12 10:48:04 +0330

    Solve day12 problem

--- bin/adventofcode.dart
+++ bin/adventofcode.dart
@@ -1,5 +1,6 @@
 import 'package:adventofcode/day1/day1.dart';
 import 'package:adventofcode/day11/day11.dart';
+import 'package:adventofcode/day12/day12.dart';
 import 'package:adventofcode/day2/day2.dart';
 import 'package:adventofcode/day3/day2.dart';
 import 'package:adventofcode/day4/day4.dart';
@@ -11,15 +12,16 @@ import 'package:adventofcode/day9/day9.dart';
 import 'package:adventofcode/day10/day10.dart';
 
 void main(List<String> arguments) async {
-  // print('Day1 answer is: ${await solveDay1()}');
-  // print('Day2 answer is: ${await solveDay2()}');
-  // print('Day3 answer is: ${await solveDay3()}');
-  // print('Day4 answer is: ${await solveDay4()}');
-  // print('Day5 answer is: ${await solveDay5()}');
-  // print('Day6 answer is: ${await solveDay6()}');
-  // print('Day7 answer is: ${await solveDay7()}');
-  // print('Day8 answer is: ${await solveDay8()}');
-  // print('Day9 answer is: ${await solveDay9()}');
-  // print('Day10 answer is: ${await solveDay10()}');
+  print('Day1 answer is: ${await solveDay1()}');
+  print('Day2 answer is: ${await solveDay2()}');
+  print('Day3 answer is: ${await solveDay3()}');
+  print('Day4 answer is: ${await solveDay4()}');
+  print('Day5 answer is: ${await solveDay5()}');
+  print('Day6 answer is: ${await solveDay6()}');
+  print('Day7 answer is: ${await solveDay7()}');
+  print('Day8 answer is: ${await solveDay8()}');
+  print('Day9 answer is: ${await solveDay9()}');
+  print('Day10 answer is: ${await solveDay10()}');
   print('Day11 answer is: ${await solveDay11()}');
+  print('Day12 answer is: ${await solveDay12()}');
 }
