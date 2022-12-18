f20e0a34923f739a84cc2069bc3b1a097a80a822
imaNNeoFighT <iman.neofight@gmail.com>
2022-12-18 10:04:01 +0330

    Solve day18 problem

--- bin/adventofcode.dart
+++ bin/adventofcode.dart
@@ -6,6 +6,7 @@ import 'package:adventofcode/day14/day14.dart';
 import 'package:adventofcode/day15/day15.dart';
 import 'package:adventofcode/day16/day16.dart';
 import 'package:adventofcode/day17/day17.dart';
+import 'package:adventofcode/day18/day18.dart';
 import 'package:adventofcode/day2/day2.dart';
 import 'package:adventofcode/day3/day2.dart';
 import 'package:adventofcode/day4/day4.dart';
@@ -34,4 +35,5 @@ void main(List<String> arguments) async {
   print('Day15 answer is: ${await solveDay15()}');
   print('Day16 answer is: ${await solveDay16()}');
   print('Day17 answer is: ${await solveDay17()}');
+  print('Day18 answer is: ${await solveDay18()}');
 }
---
+++ lib/day18/day18.dart
@@ -0,0 +1,98 @@
+import 'dart:io';
+
+import 'package:adventofcode/utils/collection.dart';
+import 'package:equatable/equatable.dart';
+
+Future<String> solveDay18() async {
+  final inputFile = File('lib/day18/input.txt');
+  final input = await inputFile.readAsString();
+
+  final List<Cube> cubes = [];
+  for (final line in input.split('\n')) {
+    int x = int.parse(line.split(',')[0]);
+    int y = int.parse(line.split(',')[1]);
+    int z = int.parse(line.split(',')[2]);
+    cubes.add(Cube(x, y, z));
+  }
+
+  return 'part1: ${_part1(cubes)}, part2: ${_part2(cubes)}';
+}
+
+int _part1(List<Cube> cubes) {
+  int sides = 0;
+  for (final cube in cubes) {
+    sides += 6;
+    for (final adjacent in cube.adjacentCubes) {
+      if (cubes.contains(adjacent)) {
+        sides -= 1;
+      }
+    }
+  }
+  return sides;
+}
+
+int _part2(List<Cube> cubes) {
+  final sorted = cubes.toList();
+  sorted.sort((a, b) => (a.x + a.y + a.z).compareTo(b.x + b.y + b.z));
+  final minimum = sorted.first;
+
+  late Cube firstAir;
+  for (final d in minimum.adjacentCubes) {
+    if (!cubes.contains(d)) {
+      firstAir = d;
+    }
+  }
+
+  List<Cube> queue = [firstAir];
+  Set<Cube> airBlocks = {};
+  while (queue.isNotEmpty) {
+    final current = queue.removeAt(0);
+    airBlocks.add(current);
+    for (final adjacent in current.adjacentCubes) {
+      if (airBlocks.contains(adjacent) ||
+          cubes.contains(adjacent) ||
+          queue.contains(adjacent)) {
+        continue;
+      }
+      if (_shortestDistance(cubes, adjacent) > 2) {
+        continue;
+      }
+      queue.add(adjacent);
+    }
+  }
+
+  int surfaceArea = 0;
+  for (final airBlock in airBlocks) {
+    for (final adjacent in airBlock.adjacentCubes) {
+      if (cubes.contains(adjacent)) {
+        surfaceArea++;
+      }
+    }
+  }
+
+  return surfaceArea;
+}
+
+int _shortestDistance(List<Cube> cubes, Cube target) => cubes.map((cube) {
+      return (target.x - cube.x).abs() +
+          (target.y - cube.y).abs() +
+          (target.z - cube.z).abs();
+    }).min();
+
+class Cube with EquatableMixin {
+  final int x, y, z;
+
+  List<Cube> get adjacentCubes => [
+        Cube(x + 1, y, z),
+        Cube(x - 1, y, z),
+        Cube(x, y + 1, z),
+        Cube(x, y - 1, z),
+        Cube(x, y, z + 1),
+        Cube(x, y, z - 1),
+      ];
+
+  Cube(this.x, this.y, this.z);
+
+  @override
+  List<Object?> get props => [x, y, z];
+}
